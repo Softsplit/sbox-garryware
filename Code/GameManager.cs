847fa0f4cb7256f901ba0071d9ef4b464b17624f
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2024-12-18 01:11:36 +0800

    Revert "Implement round system (could be better)"
    This reverts commit 41b5a4ce6e2eacbd8fe2086b289688bf3e23f514.

--- Code/GameManager.cs
+++ Code/GameManager.cs
@@ -4,19 +4,6 @@ public sealed class GameManager : GameObjectSystem<GameManager>, Component.INetw
 	{
 	}
 
-	public enum RoundState
-	{
-		WaitingForPlayers,
-		Intermission,
-		RoundActive
-	}
-
-	public float IntermissionDuration => 30.0f;
-	public float RoundDuration => 300.0f;
-
-	public int MinimumPlayers { get; set; } = 2;
-	private RoundState CurrentRoundState { get; set; } = RoundState.Intermission;
-
 	void ISceneStartup.OnHostPreInitialize( SceneFile scene )
 	{
 		Log.Info( $"Walker: Loading scene {scene.ResourceName}" );
@@ -24,150 +11,23 @@ public sealed class GameManager : GameObjectSystem<GameManager>, Component.INetw
 
 	void ISceneStartup.OnHostInitialize()
 	{
-		// Existing code
+		//
+		// TODO: We don't have a menu, but if we did we could put a special component in the menu
+		// scene that we'd now be able to detect, and skip doing the stuff below.
+		//
+
+		//
+		// Spawn the engine scene.
+		// This scene is sent to clients when they join.
+		//
 		var slo = new SceneLoadOptions();
 		slo.IsAdditive = true;
 		slo.SetScene( "scenes/engine.scene" );
 		Scene.Load( slo );
 
+		// If we're not hosting a lobby, start hosting one
+		// so that people can join this game.
 		Networking.CreateLobby();
-
-		// Start the game loop asynchronously
-		_ = RunGameLoopAsync();
-	}
-
-	private async Task RunGameLoopAsync()
-	{
-		while ( true )
-		{
-			switch ( CurrentRoundState )
-			{
-				case RoundState.WaitingForPlayers:
-					Log.Info( "Waiting for players..." );
-					while ( GetPlayerCount() < MinimumPlayers )
-					{
-						await GameTask.Delay( 1000 );
-					}
-					TransitionToState( RoundState.Intermission );
-					break;
-
-				case RoundState.Intermission:
-					Log.Info( "Intermission has started." );
-					RespawnAllPlayersInLobby();
-					await GameTask.Delay( (int)(IntermissionDuration * 1000) );
-					if ( GetPlayerCount() >= MinimumPlayers )
-					{
-						TransitionToState( RoundState.RoundActive );
-					}
-					else
-					{
-						TransitionToState( RoundState.WaitingForPlayers );
-					}
-					break;
-
-				case RoundState.RoundActive:
-					Log.Info( "Round has started!" );
-					RespawnAllPlayersForRound();
-					var roundEndTime = Time.Now + RoundDuration;
-					while ( Time.Now < roundEndTime )
-					{
-						if ( GetPlayerCount() < MinimumPlayers )
-						{
-							break;
-						}
-						await GameTask.Delay( 1000 );
-					}
-					TransitionToState( RoundState.Intermission );
-					break;
-			}
-			await GameTask.Delay( 100 ); // Small delay to prevent tight loop
-		}
-	}
-
-	void TransitionToState( RoundState newState )
-	{
-		OnLeaveState( CurrentRoundState );
-		CurrentRoundState = newState;
-		OnEnterState( CurrentRoundState );
-	}
-
-	void OnEnterState( RoundState state )
-	{
-		switch ( state )
-		{
-			case RoundState.WaitingForPlayers:
-				Log.Info( "Waiting for players..." );
-				break;
-
-			case RoundState.Intermission:
-				Log.Info( "Intermission has started." );
-				RespawnAllPlayersInLobby();
-				break;
-
-			case RoundState.RoundActive:
-				Log.Info( "Round has started!" );
-				RespawnAllPlayersForRound();
-				break;
-		}
-	}
-
-	void OnLeaveState( RoundState state )
-	{
-		// Cleanup if necessary
-	}
-
-	void RespawnAllPlayersInLobby()
-	{
-		foreach ( var player in Scene.GetAllComponents<Player>() )
-		{
-			if ( player.IsValid() )
-			{
-				var spawnPoint = FindLobbySpawnLocation();
-				player.GameObject.WorldTransform = spawnPoint;
-				player.Health = 100;
-			}
-		}
-	}
-
-	void RespawnAllPlayersForRound()
-	{
-		foreach ( var player in Scene.GetAllComponents<Player>() )
-		{
-			if ( player.IsValid() )
-			{
-				var spawnPoint = FindRoundSpawnLocation();
-				player.GameObject.WorldTransform = spawnPoint;
-				player.Health = 100;
-			}
-		}
-	}
-
-	int GetPlayerCount()
-	{
-		return Scene.GetAllComponents<Player>().Count();
-	}
-
-	Transform FindLobbySpawnLocation()
-	{
-		// Implement later
-
-		return FindSpawnLocation();
-	}
-
-	Transform FindRoundSpawnLocation()
-	{
-		return FindSpawnLocation();
-	}
-
-	Transform FindSpawnLocation()
-	{
-		var spawnPoints = Scene.GetAllComponents<SpawnPoint>().ToArray();
-		if ( spawnPoints.Length > 0 )
-		{
-			return Random.Shared.FromArray( spawnPoints ).Transform.World;
-		}
-
-		return Transform.Zero;
 	}
 
 	void Component.INetworkListener.OnActive( Connection channel )
@@ -177,8 +37,10 @@ public sealed class GameManager : GameObjectSystem<GameManager>, Component.INetw
 
 	public void SpawnPlayerForConnection( Connection channel )
 	{
-		var startLocation = FindLobbySpawnLocation().WithScale( 1 );
+		// Find a spawn location for this player
+		var startLocation = FindSpawnLocation().WithScale( 1 );
 
+		// Spawn this object and make the client the owner
 		var playerGo = GameObject.Clone( "/prefabs/player.prefab", new CloneConfig { Name = $"Player - {channel.DisplayName}", StartEnabled = true, Transform = startLocation } );
 		var player = playerGo.Components.Get<Player>( true );
 		playerGo.NetworkSpawn( channel );
@@ -186,28 +48,24 @@ public sealed class GameManager : GameObjectSystem<GameManager>, Component.INetw
 		IPlayerEvent.PostToGameObject( player.GameObject, x => x.OnSpawned() );
 	}
 
-	[ConCmd( "gw_setstate" )]
-	public static void SetRoundState( string stateName )
+
+	/// <summary>
+	/// Find the most appropriate place to respawn
+	/// </summary>
+	Transform FindSpawnLocation()
 	{
-		if ( Current == null ) return;
-
-		if ( Enum.TryParse<RoundState>( stateName, true, out var newState ) )
+		//
+		// If we have any SpawnPoint components in the scene, then use those
+		//
+		var spawnPoints = Scene.GetAllComponents<SpawnPoint>().ToArray();
+		if ( spawnPoints.Length > 0 )
 		{
-			Current.TransitionToState( newState );
-			Log.Info( $"GameManager: Round state set to {newState}" );
+			return Random.Shared.FromArray( spawnPoints ).Transform.World;
 		}
-		else
-		{
-			Log.Error( $"Invalid round state: {stateName}" );
-		}
-	}
 
-	[ConCmd( "gw_setminplayers" )]
-	public static void SetMinimumPlayers( int minPlayers )
-	{
-		if ( Current == null ) return;
-
-		Current.MinimumPlayers = minPlayers;
-		Log.Info( $"GameManager: Minimum players set to {minPlayers}" );
+		//
+		// Failing that, spawn where we are
+		//
+		return Transform.Zero;
 	}
 }
