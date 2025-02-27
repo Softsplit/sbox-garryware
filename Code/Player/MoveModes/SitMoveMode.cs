5f9e5eea9a2246af39364372ab55a864ff88bea4
trende2001 <68961063+trende2001@users.noreply.github.com>
2025-02-28 00:45:34 +0100

    Sitting mode test

---
+++ Code/Player/MoveModes/SitMoveMode.cs
@@ -0,0 +1,21 @@
+﻿
+using Sandbox.Movement;
+
+[Icon( "scuba_diving" ), Group( "Movement" ), Title( "MoveMode - Sitting" )]
+public class SitMoveMode : MoveMode
+{
+	[RequireComponent]
+	private Player Player { get; set; }
+	
+	public override int Score( PlayerController controller )
+	{
+		if ( Player.IsSitting ) return 20;
+
+		return -100;
+	}
+
+	public override void OnModeBegin()
+	{
+		Controller.Renderer.Set( "sit", 1 );	
+	}
+}
