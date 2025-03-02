5c51969727a04d7cf38e4a2e7454174d178ccfde
trende2001 <68961063+trende2001@users.noreply.github.com>
2025-03-02 12:41:43 +0100

    Networked sound point util

---
+++ Code/Utils/NetworkSound.cs
@@ -0,0 +1,38 @@
+﻿public static class NetworkSound
+{
+	[Rpc.Broadcast]
+	public static void ToggleSoundPoint( GameObject soundPointObj, bool shouldPlay )
+	{
+		var soundPoint = soundPointObj.GetComponent<SoundPointComponent>();
+		if ( !soundPoint.IsValid() )
+			return;
+
+		if ( shouldPlay )
+		{
+			soundPoint.StartSound();
+		}
+		else
+		{
+			soundPoint.StopSound();
+		}
+	}
+}
+
+public static class SoundPointExtensions
+{
+	public static void BroadcastStart( this SoundPointComponent soundPoint )
+	{
+		if ( !soundPoint.IsValid() )
+			return;
+
+		NetworkSound.ToggleSoundPoint( soundPoint.GameObject, true );
+	}
+
+	public static void BroadcastStop( this SoundPointComponent soundPoint )
+	{
+		if ( !soundPoint.IsValid() )
+			return;
+
+		NetworkSound.ToggleSoundPoint( soundPoint.GameObject, false );
+	}
+}
